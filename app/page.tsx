8de1de23a522b0f0d12d1437f8a8f3ddde9d6161
felipeFerreiraffl <felipado2x0@gmail.com>
2026-01-30 20:08:22 -0300

    style: added tailwind utilities into page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <section>
-      <div>
+    <section className="flex flex-1 flex-col justify-center items-center h-dvh gap-10">
+      <div className="flex flex-col items-center">
         <h1 className="typo-display">Projeto Placeholder</h1>
-        <p>Simples e direto com JSON Placeholder API</p>
+        <p className="typo-lg-sbttl">
+          Simples e direto com JSON Placeholder API
+        </p>
       </div>
-      <div></div>
+      <div className="flex items-center gap-5">
+        <Link href={"/posts"} className="link-button typo-md-btn">
+          Posts
+        </Link>
+        <Link href={"/users"} className="link-button typo-md-btn">
+          Usuários
+        </Link>
+        <Link href={"/albums"} className="link-button typo-md-btn">
+          Álbuns
+        </Link>
+      </div>
     </section>
   );
 }
