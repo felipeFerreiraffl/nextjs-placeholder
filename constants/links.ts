0258c98cf648297c9b797092cd768298461d41f2
felipeFerreiraffl <felipado2x0@gmail.com>
2026-01-31 14:43:50 -0300

    feat: created new folder for project constants and added file to centralize all the links

---
+++ constants/links.ts
@@ -0,0 +1,10 @@
+export const SOCIAL_LINKS = {
+  social: {
+    github: "https://github.com/felipeFerreiraffl",
+  },
+  pages: {
+    posts: "/posts",
+    users: "/users",
+    albums: "/albums",
+  },
+} as const;
